4e1cf1624530cdd1480074fd46ba75afbc5970b5
Fer Catalano <fernando@hazak.tech>
2024-04-25 16:00:49 -0300

    Add - modal banner promo

--- inc/functions/ajax-requests.php
+++ inc/functions/ajax-requests.php
@@ -16,13 +16,13 @@ if( !empty($_POST) && isset($_POST['action']) && $_POST['action'] == 'actionLogi
     $login = 'false';
     $updated = false;
 
-    // $request = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".Store::get_google_api()."&response=".$recaptcha."&remoteip=".$_SERVER['REMOTE_ADDR']);
-    // $response = json_decode($request);
+    $request = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".Store::get_google_api()."&response=".$recaptcha."&remoteip=".$_SERVER['REMOTE_ADDR']);
+    $response = json_decode($request);
     
-    // if ( $response->success === false ) {
-    //     echo 'Captcha Incorrecto!';
-    //     die();
-    // }
+    if ( $response->success === false ) {
+        echo 'Captcha Incorrecto!';
+        die();
+    }
 
     $access = new Login($user, $pass);
     $result = $access->loginProcess();
@@ -494,6 +494,7 @@ if( !empty($_POST) && isset($_POST['action']) && $_POST['action'] == 'operationC
     $response  = 0;
     $logo = null;
     $banner = null;
+    $promo_modal = null;
     $email = (isset($_POST['email']) ? filter_var($_POST['email'], FILTER_SANITIZE_EMAIL) : null);
     $telefono = (isset($_POST['telefono']) ? filter_var($_POST['telefono'], FILTER_UNSAFE_RAW) : null);
     $atencion = (isset($_POST['atencion']) ? filter_var($_POST['atencion'], FILTER_UNSAFE_RAW) : null);
@@ -547,9 +548,28 @@ if( !empty($_POST) && isset($_POST['action']) && $_POST['action'] == 'operationC
             }
         }
 
+        if(isset($_FILES['promo_modal']['name'])){
+            $filename = $_FILES['promo_modal']['name'];
+            $prefijo = substr(md5(uniqid(rand())),0,6);
+            $path = "img/config/".$prefijo.'_'.$filename;
+            $location = "../../img/config/".$prefijo.'_'.$filename;
+            $imageFileType = pathinfo($location,PATHINFO_EXTENSION);
+            $imageFileType = strtolower($imageFileType);
+            // Valid extensions
+            $valid_extensions = array("jpg","jpeg","png","webp");
+            // Check file extension
+            if(in_array(strtolower($imageFileType), $valid_extensions)) {
+                // Upload file
+                if(move_uploaded_file($_FILES['promo_modal']['tmp_name'],$location)){
+                    $promo_modal = $path;
+                }
+            }
+        }
+
         $general = new Configuracion();
         if ($logo) $general->logo = $logo;
         if ($banner) $general->banner = $banner;
+        if ($promo_modal) $general->promo_modal = $promo_modal;
         $general->telefono = $telefono;
         $general->email = $email;
         $general->direccion = $direccion;
@@ -564,6 +584,21 @@ if( !empty($_POST) && isset($_POST['action']) && $_POST['action'] == 'operationC
         $general->show_prices = $show_prices == '1' ? 1 : 0;
         $general->show_instagram = $show_instagram == '1' ? 1 : 0;
         $general->update();
+        die('true');
+
+    } catch (Exception $e) {
+        die('false');
+    }
+}
+
+/**
+ * Request of Remove Promo Banner
+ */
+if( !empty($_POST) && isset($_POST['action']) && $_POST['action'] == 'operationRemovePromoBanner') {
+
+    try {
+        $general = new Configuracion();
+        $general->deletePromoBanner();
         die('true');
 
     } catch (Exception $e) {
