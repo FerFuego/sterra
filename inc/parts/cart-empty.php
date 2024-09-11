1a23ffa3eec802415e9b65f7bd78c8dcad4c658d
Luciano Colmano <lcolmano@gmail.com>
2024-09-11 07:44:00 -0300

    corrijo leyenda cart-empty

--- inc/parts/cart-empty.php
+++ inc/parts/cart-empty.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-12">
-            <h2 class="text-danger text-center">No tienes ningun pedido abierto</h2>
+            <h2 class="text-danger text-center">No tienes ningún pedido abierto. Muy pronto podrás realizar el pedido desde el sitio web.</h2>
         </div>
     </div>
 </div>
