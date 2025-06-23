7174ce4d116c61102ea670aa37a7c3d15c6ba76d
Fer Catalano <fernando@hazak.tech>
2025-06-23 10:56:46 -0300

    text price cart

--- inc/partials/item-cart.php
+++ inc/partials/item-cart.php
@@ -12,13 +12,13 @@
             <textarea type="text" name="nota" id="nota_<?php echo $product->Auto; ?>"><?php echo $product->Notas; ?></textarea>
         </td>
         <td class="shoping__cart__price">
-            $<?php echo number_format(Productos::PreVtaFinal($product->PreVtaFinal1), 2,',','.'); ?>
+            $<?php echo number_format($product->PreVtaFinal1, 2,',','.'); ?>
         </td>
         <td class="shoping__cart__quantity">
             <div class="quantity">
                 <div class="pro-qty">
-                    <?php $prod = new Productos($product->CodProducto); ?>
-                    <input type="number" name="cant" max="<?php echo $prod->getStock(); ?>" id="cant_<?php echo $product->Auto; ?>" value="<?php echo $product->Cantidad; ?>">
+                    <?php //$prod = new Productos($product->CodProducto); ?>
+                    <input type="number" name="cant" max="<?php //echo $prod->getStock(); ?>" id="cant_<?php echo $product->Auto; ?>" value="<?php echo $product->Cantidad; ?>">
                 </div>
             </div>
         </td>
